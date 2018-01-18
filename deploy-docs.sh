60a689f7e8eacd3bb26bcf402f184d5186cca1d9
Paul Ryan <paul@simplycomplex.co>
2018-01-18 14:30:48 -0500

    docs: Builder

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -5,11 +5,11 @@ NC='\033[0m' # No Color
 
 if [[ -z "${STATUS// }" ]]
 then
-    node_modules/.bin/cpx -r documentation/* demo/dist/api/
+    cp -r documentation/* demo/dist
     sed -i "" '/\/dist/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix demo/dist/api master`:gh-pages --force
+    git push origin `git subtree split --prefix demo/dist master`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
