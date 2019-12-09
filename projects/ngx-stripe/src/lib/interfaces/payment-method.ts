109fe4e02880dc2fc0affc251dd298d801ff04a3
Paul Ryan <paul@simplycomplex.co>
2019-12-08 17:26:27 -0800

    filled in missing parts of type definitions.

--- projects/ngx-stripe/src/lib/interfaces/payment-method.ts
+++ projects/ngx-stripe/src/lib/interfaces/payment-method.ts
@@ -6,6 +6,13 @@ export interface PaymentMethodParams {
   type: 'card' | 'ideal' | 'sepa_debit';
   card: Element;
   billing_details: BillingDetails;
+  ideal?: {
+    bank?: string;
+  };
+  metadata?: { [key: string]: any };
+  sepa_debit?: {
+    iban: string
+  };
 }
 
 export interface PaymentMethodResult {
@@ -15,40 +22,45 @@ export interface PaymentMethodResult {
 
 export interface PaymentMethodData {
   id: string;
-  object: string;
+  object: 'payment_method';
   billing_details: BillingDetails;
   card: {
     brand: 'amex' | 'discover' | 'jcb' | 'mastercard' | 'unionpay' | 'visa' | 'unknown';
     checks: {
-        address_line1_check: string;
-        address_postal_code_check: string;
-        cvc_check: string;
-    }
+        address_line1_check: 'pass' | 'fail' | 'unavailable' | 'unchecked';
+        address_postal_code_check: 'pass' | 'fail' | 'unavailable' | 'unchecked';
+        cvc_check: 'pass' | 'fail' | 'unavailable' | 'unchecked';
+    };
     country: string;
     exp_month: number;
     exp_year: number;
     fingerprint: string;
     funding: 'credit' | 'debit' | 'prepaid' | 'unknown';
-    generated_from: any;
+    generated_from: {
+      charge: string;
+      payment_method_details: any;
+    };
     last4: string;
     three_d_secure_usage: {
         supported: boolean;
-    }
+    };
     wallet: any;
   };
   card_present: any;
+  created: number;
   customer: string;
   ideal: {
-      bank: string;
+      bank: 'abn_amro' | 'asn_bank' | 'bunq' | 'handelsbaknken' | 'ing' | 'knab' | 'moneyou' | 'rabobank' | 'regiobank' | 'sns_bank' | 'triodos_bank' | 'van_lanschot';
       bic: string;
   };
   livemode: boolean;
-  metadata: any;
+  metadata: { [key: string]: any };
   sepa_debit: {
       bank_code: string;
       branch_code: string;
       country: string;
       fingerprint: string;
       last4: string;
-  }
+  };
+  type: 'card' | 'ideal' | 'sepa_debit';
 }
